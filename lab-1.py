97debf3f1cacde65513586a7d19739af89b1826e
FolderN <cnu4ki41@gmail.com>
2022-03-17 19:47:38 +0700

    Initial Commit

--- lab-1.py
+++ lab-1.py
@@ -1,13 +1,18 @@
 import math
 
-def func():
+
+def putin():
     print("Введите x.")
     try:
         x = float(input())
     except ValueError:
         print("Неверный формат данных, завершение программы.")
         exit()
+    return x
 
+
+def func():
+    x = putin()
     if 5 <= x < 10:
         return 1 - math.sin(x)
     elif 10 <= x < 15:
@@ -16,11 +21,13 @@ def func():
         return math.tan(x)
     elif 20 <= x < 25:
         return 1/math.tan(x)
-    else: return 0
+    else:
+        return 0
 
 
 def main():
     print(f"Результат y(x): {func()}")
+
 
 if __name__ == "__main__":
     main()
